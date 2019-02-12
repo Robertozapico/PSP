aabe9cecb6bcd8b914a9185e4544fe23932651cb
RobertoZapico <robertozapicoblanco@gmail.com>
2019-02-12 12:28:38 +0100

    funciona a medias

--- Eval2MandarImagen/src/MainEmisor.java
+++ Eval2MandarImagen/src/MainEmisor.java
@@ -1,5 +1,14 @@
 
 import aplicacion.PersonaRoberto;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,15 +25,27 @@ public class MainEmisor {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Emisor emisor = new Emisor();
-        PersonaRoberto persona = new PersonaRoberto();
+    public static void main(String[] args) throws FileNotFoundException, SocketException, IOException{
+        File imagen = new File ("/home/alumnop/Documentos/Proyectos/PSP1819/Eval2MandarImagen/src/img/pmdm.png");
+        byte[] buffer = new byte[4096];
         
-        System.out.println(emisor.getClass().getTypeName());
-        persona.setNombre("Paco");
-        persona.setEdad(25);
-            
-        emisor.enviarObjeto("localhost", persona);
+        FileInputStream fis = new FileInputStream(imagen);
+        DataInputStream dis = new DataInputStream(fis);
+        
+        int numPaquetes=(int) (imagen.length()/buffer.length);
+        DatagramSocket server = new DatagramSocket();
+
+        DatagramPacket DGP= new DatagramPacket(buffer, buffer.length, InetAddress.getByName("127.0.0.1"), 2000);
+        DGP.setLength(numPaquetes);
+        server.send(DGP);
+        DGP.setLength(buffer.length);
+        
+        
+        
+        for (int i = 0; i < numPaquetes; i++) {
+            fis.read(buffer, 0, buffer.length);
+            server.send(DGP);
+        }
         
         
     }
--- Eval2MandarImagen/src/MainReceptor.java
+++ Eval2MandarImagen/src/MainReceptor.java
@@ -1,3 +1,13 @@
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,9 +23,26 @@ public class MainReceptor {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Receptor receptor = new Receptor();
-        receptor.recibirDatagrama();
+    public static void main(String[] args) throws FileNotFoundException, SocketException, IOException {
+        
+        File imagenDescargada = new File("imagenDescargada.png");
+        byte[] buffer = new byte[4096];
+        
+        FileOutputStream fos = new FileOutputStream(imagenDescargada);
+        
+        DatagramSocket cliente = new DatagramSocket(2000);
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        cliente.receive(packet);
+        int numPaquetes = packet.getLength();
+        
+        for (int i = 0; i < numPaquetes; i++) {
+            cliente.receive(packet);
+            fos.write(buffer);
+            
+            
+        }
+        fos.close();
+        Desktop.getDesktop().open(imagenDescargada);
     }
 
 }
